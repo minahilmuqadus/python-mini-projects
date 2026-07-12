a019132201c7a184730688c784c74cae188dae03
Minahil <minahilmuqadus7@gmail.com>
2026-07-12 17:25:53 +0500

    Improve Student Grade Analyzer

--- student_grade_analyzer.py
+++ student_grade_analyzer.py
@@ -5,6 +5,20 @@ chemistry_marks =float(input("Enter chemistry marks: "))
 def calculate_average  (maths_marks , physics_marks , chemistry_marks):
     average= (maths_marks + physics_marks + chemistry_marks)/3
     return average
-
+print("================")
 average = calculate_average(maths_marks , physics_marks , chemistry_marks)
 print ("Average marks: " , average)
+if average>=90:
+   print("Grade A+")
+elif average>=80:
+  print("Grade A")
+elif average>=70:
+  print("Grade B")
+elif average>=60:
+   print("Grade C")
+elif average>=50:
+   print("Grade D")
+else:
+   print("Grade F")
+
+print("================")
